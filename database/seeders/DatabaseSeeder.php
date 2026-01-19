1a5fce6cb92fcd98343de63e051d0a910dea4da8
Henrik Schumacher <henrik.schumacher@cre8-it.de>
2026-01-19 17:30:33 +0100

    chore: update dummy user credentials in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
 use App\Models\User;
@@ -17,7 +19,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@user.com',
+            'password' => 'password',
         ]);
     }
 }
